e5d3a62b932ab24eaf0125691f04916664738708
Greatwolf <github.greatwolf@mamber.net>
2014-05-27 13:08:21 -0700

    Extracted auth info for tests into 'tests/api_testkeys.lua'.

---
+++ tests/api_testkeys.lua
@@ -0,0 +1,15 @@
+-- api authentication info used for exchange unit tests
+return 
+{
+  poloniex = 
+  {
+    key = "",
+    secret = ""
+  },
+
+  mintpal = 
+  {
+    session_id = "",
+    incap_ses_210_152968 = "",
+  }
+}
--- tests/testpoloniex.lua
+++ tests/testpoloniex.lua
@@ -1,62 +1,61 @@
 require 'luarocks_path'
 require 'pl.app'.require_here ".."
-local api = require 'poloniex'
+local api = require 'exchange.poloniex'
 local dump = require 'pl.pretty'.dump
 
-local apikey = "89QK21JO-8O72SN4U-ZUR4CGE5-TSGNT63S"
-local apisecret = "50c94254737f8a47364eb6825055fd93c5cf9b2e927d366e7acdf22e58e89e3fa2c32955d1feddc2f395a2d44507c36167be348dbd44b3eae6d1019d938994cd"
-session = api { key = apikey, secret = apisecret }
+
+local keys = require 'tests.api_testkeys'.poloniex
+session = api { key = keys.key, secret = keys.secret }
+assert (session)
 
 local tests = 
 {
   test_balance = function ()
     local r = session:balance()
 
-    assert (r.BTC == "0.0")
-    assert (r.LTC == "0.0")
-    assert (r.DRK == "0.0")
+    dump (r)
   end,
 
   test_tradehistory = function ()
     local r = session:tradehistory("BTC", "LTC")
 
-    assert (not r)
+    dump (r)
   end,
 
   test_buy = function ()
     local r = session:buy("BTC", "LTC", 0.00015, 1)
 
-    assert (r.error == "Not enough BTC.")
+    dump (r)
   end,
 
   test_sell = function ()
     local r = session:sell("BTC", "LTC", 0.15, 1)
 
-    assert (r.error == "Not enough LTC.")
+    dump (r)
   end,
 
   test_cancelorder = function ()
     local r = session:cancelorder("BTC", "LTC", 170675)
 
-    assert (r.error:match "Invalid order number")
+    dump (r)
   end,
 
   test_markethistory = function ()
     local r = session:markethistory("BTC", "LTC")
 
-    -- dump (r)
-    assert (next(r))
+    assert (r)
   end,
 
   test_orderbook = function ()
     local r = session:orderbook("BTC", "LTC")
 
-    assert (next(r))
+    dump (r)
   end,
   
   test_openorders = function ()
-    local r = session:openorders("BTC", "LTC")
-    assert (not r)
+    local r = session:openorders("BTC", "CINNI")
+
+    dump (r)
   end
 }
 
@@ -80,5 +79,4 @@ function run_tests (tests)
   print ("  --> " .. testfail .. " out of " .. testcount .. " test(s) failed.")
 end
 
--- run_tests (tests)
-run_single ("test_cancelorder", tests.test_cancelorder)
+run_tests (tests)
