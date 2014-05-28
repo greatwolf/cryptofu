f29a17107ffce2bd98e1c2e14c8f7659b4e841d5
Greatwolf <github.greatwolf@mamber.net>
2014-05-27 21:03:17 -0700

    Preliminary implementation of arbitration.

---
+++ arbitrate.lua
@@ -0,0 +1,91 @@
+local tablex = require 'pl.tablex'
+
+local isempty = function (t)
+  return not next (t)
+end
+
+local function compute_arb (orderbook1, orderbook2)
+--[[ arbitration algorithm
+orderbook 1               orderbook 2
+buy                       sell
+                            0.02018989	0.00015972	
+                            0.02016002	1.47555059	
+                            0.02016001	6.35601551	
+                            0.02015999	3.94617200	
+                            0.02014996	8.32493600	
+                            0.02014992	66.04248947	
+                            0.02014990	39.63971162	->> 39.02813755 ->> 21.45727557 ->> 13.62995628
+
+0.02040001	0.61086245	x
+0.02040000	17.57086198	x
+0.02020000	7.82731929	x
+0.02010000	57.32231791	
+0.01985023	3.07442252	
+
+
+while sell_pos.price < buy_pos.price
+  last_sellprice = sell_pos.price
+  last_buyprice = buy_pos.price
+  if sell_pos.amount < buy_pos.amount
+    accum += sell_pos.amount
+    buy_pos.amount -= sell_pos.amount
+    sell_pos++
+  else 
+    accum += buy_pos.amount
+    sell_pos.amount -= buy_pos.amount
+    buy_pos++
+  
+buy/sell amount
++0.61086245
++17.57086198
++7.82731929
+
+final output:
+  buy 26.00904372 @0.02014990
+  sell 26.00904372 @0.02020000
+--]]
+  local ask_amounts = tablex.icopy ({}, orderbook2.sell.amount)
+  local bid_amounts = tablex.icopy ({}, orderbook1.buy.amount)
+
+  local sells, buys = orderbook2.sell.price, orderbook1.buy.price
+  local ask_index, bid_index = 1, 1
+  local last_askprice, last_bidprice
+  local totalamount = 0
+  while sells[ask_index] < buys[bid_index] do
+    last_askprice, last_bidprice = sells[ask_index], buys[bid_index]
+    if ask_amounts[ask_index] < bid_amounts[bid_index] then
+      totalamount = totalamount + ask_amounts[ask_index]
+      bid_amounts[bid_index] = bid_amounts[bid_index] - ask_amounts[ask_index]
+      ask_index = ask_index + 1
+    else
+      totalamount = totalamount + bid_amounts[bid_index]
+      ask_amounts[ask_index] = ask_amounts[ask_index] - bid_amounts[bid_index]
+      bid_index = bid_index + 1
+    end
+  end
+  return totalamount > 0 and
+  {
+    amount = totalamount, 
+    {rate = last_bidprice, order_type = "sell"}, 
+    {rate = last_askprice, order_type = "buy"},
+  }
+end
+
+local function arbitrate (orderbook1, orderbook2)
+  if not isempty (orderbook1.buy.price) and
+     not isempty (orderbook2.sell.price) and
+     orderbook2.sell.price[1] < orderbook1.buy.price[1] then
+    return compute_arb (orderbook1, orderbook2)
+  end
+
+  if not isempty (orderbook2.buy.price) and
+     not isempty (orderbook1.sell.price) and
+     orderbook1.sell.price[1] < orderbook2.buy.price[1] then
+    local r = arbitrate (orderbook2, orderbook1)
+    if r then r[1], r[2] = r[2], r[1] end
+    return r
+  end
+  return false
+end
+
+return arbitrate
---
+++ tests/testarbitrate.lua
@@ -0,0 +1,123 @@
+require 'luarocks_path'
+require 'pl.app'.require_here ".."
+local arbitrate = require 'arbitrate'
+local utest = require 'unittest'
+
+local orderbook1 =
+{
+  buy =
+  {
+    price = 
+    {
+      0.02040001,
+      0.02040000,
+      0.02020000,
+      0.02010000,
+      0.01985023,
+    },
+    amount = 
+    {
+      0.61086245,
+      17.57086198,
+      7.82731929,
+      57.32231791,
+      3.07442252,
+    }
+  },
+  sell = { price = {0.02050001,}, amount = {0.31086,} }
+}
+local orderbook2 =
+{
+  buy = { price = {0.01995,}, amount = {0.016,} },
+  sell =
+  {
+    price = 
+    {
+      0.02014990,
+      0.02014992,
+      0.02014996,
+      0.02015999,
+      0.02016001,
+      0.02016002,
+      0.02018989,
+    },
+    amount = 
+    {
+      39.63971162,
+      66.04248947,
+      8.32493600,
+      3.94617200,
+      6.35601551,
+      1.47555059,
+      0.00015972,
+    }
+  }
+}
+
+local tests = 
+{
+  test_arbitrate = function ()
+    local r = arbitrate (orderbook1, orderbook2)
+    assert (r[1].order_type == "sell")
+    assert (r[1].rate == 0.02020000)
+    assert (r[2].order_type == "buy")
+    assert (r[2].rate == 0.02014990)
+    assert (r.amount == 26.00904372)
+  end,
+
+  test_reverse_arbitrate = function ()
+    local r = arbitrate (orderbook2, orderbook1)
+    assert (r[2].order_type == "sell")
+    assert (r[2].rate == 0.02020000)
+    assert (r[1].order_type == "buy")
+    assert (r[1].rate == 0.02014990)
+    assert (r.amount == 26.00904372)
+  end,
+
+  test_empty_arbitrate = function ()
+    local r = arbitrate ({buy = {price ={}, amount = {}}, sell = {price ={}, amount = {}}},
+                         {buy = {price ={}, amount = {}}, sell = {price ={}, amount = {}}})
+    assert (not r)
+  end,
+
+  test_no_arbitrate = function ()
+    local orderbook1 =
+    {
+      buy =
+      {
+        price = 
+        {
+          0.02010000,
+          0.01985023,
+        },
+        amount = 
+        {
+          57.32231791,
+          3.07442252,
+        }
+      },
+      sell = { price = {0.02050001,}, amount = {0.31086,} }
+    }
+    local orderbook2 =
+    {
+      buy = { price = {0.01995,}, amount = {0.016,} },
+      sell =
+      {
+        price = 
+        {
+          0.02016002,
+          0.02018989,
+        },
+        amount = 
+        {
+          1.47555059,
+          0.00015972,
+        }
+      }
+    }
+    local r = arbitrate (orderbook1, orderbook2)
+    assert (not r)
+  end,
+}
+
+utest.run (tests)
