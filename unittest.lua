adf202d3d7f4609ed293a972c7e80f7af8be62f8
Greatwolf <github.greatwolf@mamber.net>
2014-06-04 16:20:34 -0700

    Added timings for unit test to the framework. Changed to show final timing in milliseconds if < 2 seconds.

--- unittest.lua
+++ unittest.lua
@@ -1,24 +1,37 @@
+local clock = os.clock
+local print, pcall = print, pcall
+
 local function run_single (testgroup, testname)
   assert (testgroup[testname])
   io.write ("Running " .. testname .. "... ")
+  local start = clock()
   local noerr, errmsg = pcall (testgroup[testname])
+  local elapse = clock() - start
+  elapse = elapse < 2 and string.format ("%.2fms", elapse * 1000)
+                       or string.format ("%.2fs", elapse)
+
+  io.write (elapse)
   if noerr then
-    print "ok"
+    print " ok"
   else
-    print ("fail!\n", errmsg)
+    print (" fail!\n", errmsg)
   end
   return noerr
 end
 
 local function run (tests)
   local testcount, testfail = 0, 0
-  local start = os.clock()
+  local start = clock()
   for testname in pairs(tests) do
     if not run_single (tests, testname) then testfail = testfail + 1 end
     testcount = testcount + 1
   end
-  local elapse = (os.clock() - start) * 1000 .. "ms"
-  print ("", elapse, "--> " .. testfail .. " out of " .. testcount .. " test(s) failed.")
+  local elapse = clock() - start
+  elapse = elapse < 2 and string.format ("%.2fms", elapse * 1000)
+                       or string.format ("%.2fs", elapse)
+
+  print (string.format ("    %s --> %d out of %d test(s) failed.",
+                        elapse, testfail, testcount))
 end
 
 return { run = run, run_single = run_single }
