2835cdab03af5fa6b044554a9d8690eac2eb9275
Greatwolf <github.greatwolf@mamber.net>
2017-03-04 23:43:35 -0800

    Implemented trading api for bitfinex. Switched to v1 api since v2 is unreliable especially auth.

--- exchange/bitfinex.lua
+++ exchange/bitfinex.lua
@@ -1,10 +1,31 @@
 local apiquery       = require 'tools.apiquery'
+local nonce          = require 'tools.util'.nonce
 local urlencode_parm = require 'tools.util'.urlencode_parm
-local tablex = require 'pl.tablex'
-local dump = require 'pl.pretty'.dump
+local tablex  = require 'pl.tablex'
+local hmac    = require 'crypto'.hmac
+local json    = require 'dkjson'
+local base64  = require 'basexx'.to_base64
 
-local apiv = "/v2"
+
+local apiv = "/v1"
 local url = "https://api.bitfinex.com" .. apiv
+
+local function bitfinex_authquery (self, cmd, parm)
+  parm = parm or {}
+  parm.request = apiv .. cmd
+  parm.nonce   = tostring (nonce ())
+
+  local post_data = base64 (json.encode (parm))
+  self.headers['X-BFX-PAYLOAD']   = post_data
+  self.headers['X-BFX-SIGNATURE'] = hmac.digest ("sha384", post_data, self.secret)
+
+  local res = apiquery.postrequest (url, cmd, self.headers)
+  if res[1] == "error" then
+    return nil, table.concat(res, " ")
+  end
+
+  return res
+end
 
 local bitfinex_publicquery = function (self, cmd, parm)
   parm = parm or {}
@@ -35,9 +56,8 @@ do
   mapsymbols = function (market1, market2)
     market1, market2 = market1:upper (), market2:upper ()
 
-    if symbols[market1 .. market2] then return market1 .. market2 end
     if symbols[market2 .. market1] then return market2 .. market1 end
-    return false
+    return market1 .. market2
   end
   get_marketsymbol = initsymbols
 end
@@ -45,19 +65,97 @@ end
 local bitfinex_publicapi = {}
 
 function bitfinex_publicapi:orderbook (market1, market2)
-  local cmd = "/book/t%s/P0"
-  local sym = get_marketsymbol (market1, market2) or ""
+  local cmd = "/book/%s"
+  local sym = get_marketsymbol (market1, market2)
   return bitfinex_publicquery (self, cmd:format (sym))
 end
 
 function bitfinex_publicapi:markethistory (market1, market2)
-  local cmd = "/trades/t%s/hist"
-  local sym = get_marketsymbol (market1, market2) or ""
+  local cmd = "/trades/%s"
+  local sym = get_marketsymbol (market1, market2)
   return bitfinex_publicquery (self, cmd:format (sym))
 end
 
-function bitfinex_publicapi.tradingapi (key, secret)
-  return {}
+local bitfinex_tradingapi = {}
+function bitfinex_tradingapi:balance ()
+  return self.authquery ("/balances")
 end
+
+function bitfinex_tradingapi:tradehistory (market1, market2, start_period, stop_period)
+  local parm =
+    {
+      symbol = get_marketsymbol (market1, market2),
+      timestamp = start_period or 1,
+      ['until'] = stop_period
+    }
+  local r = self.authquery ("/mytrades", parm)
+  return r
+end
+
+function bitfinex_tradingapi:buy (market1, market2, rate, quantity)
+  local parm =
+    {
+      symbol = get_marketsymbol (market1, market2),
+      amount = tostring (quantity),
+      price  = tostring (rate),
+      side   = "buy",
+      type   = "exchange limit",
+      is_postonly = true,
+    }
+  local r = self.authquery ("/order/new", parm)
+  return r
+end
+
+function bitfinex_tradingapi:sell (market1, market2, rate, quantity)
+  local parm =
+    {
+      symbol = get_marketsymbol (market1, market2),
+      amount = tostring (quantity),
+      price  = tostring (rate),
+      side   = "sell",
+      type   = "exchange limit",
+      is_postonly = true,
+    }
+  local r = self.authquery ("/order/new", parm)
+  return r
+end
+
+function bitfinex_tradingapi:cancelorder (...)
+  local order_ids = tablex.filter ({...}, function (v) return type(v) == 'number' end)
+  assert (#order_ids > 0, "no valid orderids to cancel!")
+  return self.authquery ("/order/cancel/multi", { order_ids = order_ids })
+end
+
+function bitfinex_tradingapi:openorders (market1, market2)
+  return self.authquery ("/orders")
+end
+
+local make_authself = function (apikey, apisecret)
+  return
+  {
+    headers =
+    {
+      ['X-BFX-APIKEY'] = apikey,
+      ["content-type"] = "application/json",
+      ['accept']       = "application/json",
+    },
+    secret = apisecret,
+  }
+end
+
+local make_apifactory = function (apimethods)
+  return function (apikey, apisecret)
+    assert (apikey and apisecret, "No api key/secret parameter given.")
+
+    local self        = make_authself (apikey, apisecret)
+    local new_api     = tablex.update ({}, apimethods)
+    new_api.authquery = function (...)
+                          return bitfinex_authquery (self, ...)
+                        end
+    return new_api
+  end
+end
+
+bitfinex_publicapi.tradingapi = make_apifactory (bitfinex_tradingapi)
 
 return bitfinex_publicapi
--- tests/testbitfinex.lua
+++ tests/testbitfinex.lua
@@ -1,6 +1,8 @@
 require 'pl.app'.require_here ".."
-local utest = require 'tools.unittest'
-local dump = require 'pl.pretty'.dump
+local utest   = require 'tools.unittest'
+local stack   = require 'tools.simplestack'
+local tablex  = require 'pl.tablex'
+
 
 local keys      = require 'tests.api_testkeys'.bitfinex
 local publicapi = require 'exchange.bitfinex'
@@ -25,8 +27,8 @@ utest.group "bitfinex_publicapi"
 
   test_orderbook = function ()
     local r = assert (publicapi:orderbook ("BTC", "LTC"))
-    -- dump(r)
-    assert (#r > 0)
+    assert (#r.asks > 0)
+    assert (#r.bids > 0)
   end,
 
   test_markethistory = function ()
@@ -36,8 +38,8 @@ utest.group "bitfinex_publicapi"
 
   test_bogusmarket = function ()
     local r, errmsg = publicapi:orderbook ("BTC", "___")
-    assert (not r, r)
-    assert (errmsg == "error 10020 symbol: invalid", errmsg)
+    assert (type(r) == 'table', r)
+    assert (r.message == "Unknown symbol", r.message)
   end,
 
   test_mixcasequery = function ()
@@ -48,9 +50,102 @@ utest.group "bitfinex_publicapi"
 
 local tradeapi = assert (publicapi.tradingapi (keys.key, keys.secret))
 
+local test_orders = stack ()
 utest.group "bitfinex_tradingapi"
 {
+  test_tradingapinames = function()
+    assert (not tradeapi.orderbook)
+    assert (not tradeapi.markethistory)
+
+    -- authenticated access should contain only trade functions
+    assert (tradeapi.buy)
+    assert (tradeapi.sell)
+    assert (tradeapi.cancelorder)
+    assert (tradeapi.openorders)
+    assert (tradeapi.tradehistory)
+    assert (tradeapi.balance)
+  end,
+
+  test_balance = function ()
+    local r, errmsg = assert (tradeapi:balance ())
+
+    assert (#r > 0)
+    local _, v = next (r)
+    assert (type(v.type)     == 'string')
+    assert (type(v.currency) == 'string')
+    assert (v.amount and v.available)
+    assert (not (v.amount + v.available < 0))
+  end,
+
+  test_tradehistory = function ()
+    local r = assert (tradeapi:tradehistory ("BTC", "LTC"))
+
+    assert (type(r) == 'table')
+  end,
+
+  test_buy = function ()
+    local r, errmsg = tradeapi:buy ("BTC", "USD", 0.15, 1)
+
+    assert (r.order_id, errmsg or r)
+    assert (r.side == "buy")
+    if r then
+      test_orders:push (assert (r.order_id))
+    end
+  end,
+
+  test_sell = function ()
+    local r, errmsg = tradeapi:sell ("BTC", "USD", 10000.9, 0.01)
+
+    assert (r.order_id, errmsg or r)
+    assert (r.side == "sell")
+    if r then
+      test_orders:push (assert (r.order_id))
+    end
+  end,
+}
+
+utest.group "bitfinex_orderlist"
+{
+  test_openorders = function ()
+    local r = assert (tradeapi:openorders ("USD", "BTC"))
+
+    assert (type(r) == 'table')
+    tablex.foreachi (r, function (v)
+                          assert (type(v.id) == 'number')
+                          assert (v.side == "buy" or v.side == "sell")
+                          assert (v.price)
+                          assert (v.original_amount)
+                        end)
+  end,
+}
+
+utest.group "bitfinex_cancels"
+{
+  test_cancelbadorderid = function ()
+    local ok, res = pcall (tradeapi.cancelorder, tradeapi, "BAD_ORDERNUMBER")
+    assert (not ok and res:match "no valid orderids to cancel!", res)
+  end,
+
+  test_cancelinvalidorder = function ()
+    local ok, res = pcall (tradeapi.cancelorder, tradeapi, 42)
+    assert (ok and res.result == "None to cancel", res)
+  end,
+
+  test_cancelorder = function ()
+    if test_orders:empty () then return end
+
+    assert (not test_orders:empty (), "No test orders to cancel.")
+
+    local unpack = unpack or table.unpack
+    r = assert (tradeapi:cancelorder (unpack (test_orders)))
+
+    local cancelcount = r.result:match "All %((.-)%) submitted for cancellation;"
+    assert (tonumber (cancelcount) == test_orders:size ())
+    test_orders:clear ()
+  end,
 }
 
 utest.run "bitfinex_publicapi"
 utest.run "bitfinex_tradingapi"
+utest.run "bitfinex_orderlist"
+utest.run "bitfinex_cancels"
