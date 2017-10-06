e6aed79e58790f3c167c2301da5da939d9376b68
Greatwolf <github.greatwolf@mamber.net>
2017-10-06 12:59:21 -0700

    Added 'neo' to bitfinex lending.

--- lendingbot.lua
+++ lendingbot.lua
@@ -47,13 +47,13 @@ options:
               poloniex:
                 btc, bts, clam, doge, dash, ltc, maid, str, xmr, xrp, eth, fct
               bitfinex:
-                usd, btc, bch, eth, etc, eos, zec, xmr, ltc, dsh
+                usd, btc, bch, eth, etc, eos, zec, xmr, ltc, dsh, neo
 ]]
 
 local check_currency = function (args)
   local exchange =
   {
-    bitfinex = set {"usd", "btc", "bch", "eth", "etc", "eos", "zec", "xmr", "ltc", "dsh"},
+    bitfinex = set {"usd", "btc", "bch", "eth", "etc", "eos", "zec", "xmr", "ltc", "dsh", "neo"},
     poloniex = set {"btc", "bts", "clam", "doge", "dash", "ltc",
                     "maid", "str", "xmr", "xrp", "eth", "fct"}
   }
